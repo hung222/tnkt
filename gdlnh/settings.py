3afbb5a25466ae47381d987d237336f08cee75e6
hung222 <90371118+hung222@users.noreply.github.com>
2023-02-06 16:34:56 +0700

    Update settings.py

--- gdlnh/settings.py
+++ gdlnh/settings.py
@@ -27,7 +27,7 @@ DEBUG = 'RENDER' not in os.environ
 # SECURITY WARNING: don't run with debug turned on in production!
 
 
-ALLOWED_HOSTS = ["*",]
+ALLOWED_HOSTS = []
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
